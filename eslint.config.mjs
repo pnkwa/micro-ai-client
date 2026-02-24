e6637e0305ee12910c471f44c6a0ef47a3e8f31a
pnkwa <pnkwa@MacBook-Air-khxng-pnkwa.local>
2026-02-24 16:31:17 +0700

    setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook'
 
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
@@ -38,5 +37,4 @@ export default withNuxt(
             'vue/valid-v-slot': 'off',
         },
     },
-    storybook.configs['flat/recommended'],
 )
